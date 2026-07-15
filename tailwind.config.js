3b6f2c779608c5660975446a15f167de55eeb63e
Machong Paul <59781024+sirpolux@users.noreply.github.com>
2026-07-15 13:04:13 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,28 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: {
+                    50:'#eef4ff',
+                    100:'#d9e8ff',
+                    200:'#b9d3ff',
+                    300:'#87b5ff',
+                    400:'#4b8cff',
+                    500:'#2563eb',
+                    600:'#1d4ed8',
+                    700:'#1e40af',
+                    800:'#1e3a8a',
+                    900:'#172554'
+                },
+            
+                secondary:{
+                    DEFAULT:'#0f172a'
+                },
+            
+                accent:{
+                    DEFAULT:'#f8fafc'
+                }
+            },
         },
     },
 
