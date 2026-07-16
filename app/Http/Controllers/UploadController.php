e3cf2db1c73f77ded76d2ed7a5693a1d0bc3ddc0
Machong Paul <59781024+sirpolux@users.noreply.github.com>
2026-07-16 13:42:27 +0100

    update

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUploadRequest;
+use App\Http\Requests\UpdateUploadRequest;
+use App\Models\Upload;
+
+class UploadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUploadRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Upload $upload)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Upload $upload)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUploadRequest $request, Upload $upload)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Upload $upload)
+    {
+        //
+    }
+}
